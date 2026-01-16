aebd0d2f59c0ae0a77ed4564af287ba7d7792045
KMuggz <karani.ivan25@gmail.com>
2026-01-16 21:38:58 +0300

    chore: minor edit to satisfy autochecker_3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.shortcuts import render, redirect,  get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
@@ -53,14 +53,16 @@ def add_book(request):
     return render(request, 'relationship_app/add_book.html')
 
 @permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request):
+def edit_book(request, pk=None):
     # Logic to edit a book
     return render(request, 'relationship_app/edit_book.html')
 
 @permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request):
+def delete_book(request, pk=None):
     # Logic to delete a book
     return render(request, 'relationship_app/delete_book.html')
+
+# added pk as edit/delete usually require an ID
 
 # Class-based view for library details
 class LibraryDetailView(DetailView):
