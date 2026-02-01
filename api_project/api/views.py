708ab3d2747e1fcb37b670ca3dadb47e800fc302
KMuggz <karani.ivan25@gmail.com>
2026-02-01 16:20:41 +0300

    Initial Commit: CRUD, Routers & Viewsets test.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
 
-class BookList(generics.ListAPIView):
+class BookList(generics.ListAPIView): # ListAPIView
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet): # ViewSet for CRUD operations
     queryset = Book.objects.all()
     serializer_class = BookSerializer
