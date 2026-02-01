3b62a0018ee495de6cc61112ae672ccfce52d688
KMuggz <karani.ivan25@gmail.com>
2026-02-01 17:06:10 +0300

    chore: changed  'AUTHENTIFICATION' to 'AUTHETICATION' for autochecker clarity

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTIFICATION_CLASSES': [
-        "rest_framework.authentification.TokenAuthentification",
-        "rest_framework.authentification.SessionAuthentification",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 } # This can be done using DRF’s built-in views like obtainauthtoken.
