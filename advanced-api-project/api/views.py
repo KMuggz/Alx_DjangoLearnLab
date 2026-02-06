91dede49109a2ac8cb256dcf3ae2c08cdfd57aa2
KMuggz <karani.ivan25@gmail.com>
2026-02-06 16:15:23 +0300

    Task: Implementing Filtering, Search & Ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import BookSerializer
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Book
 
 # Create your views here.
 
 # BookListView: handles GET (all books)
+"""
+updated with advanced querying:
+    - Filtering: ?title=Title&author=Name&publication_year=2020
+    - Searching: ?search=keyword (looks in title and author name)
+    - Ordering: ?ordering=title or ?ordering=-publication_year
+"""
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter] # define the backends to be used
+    filterset_fields = ['title', 'author', 'publication_year'] # filtering: exact matches for these fields
+    search_fields = ['title', 'author__name'] # searching: partial matches for title or related author's name
+    ordering_fields = ['title', 'publication_year'] # ordering: allow users to sort by these fields
+    ordering = ['title'] # default ordering
     permission_classes = [IsAuthenticatedOrReadOnly] # allows anyone to read
 
 # BookDetailView: handles GET (single book by ID)
@@ -35,3 +47,4 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
