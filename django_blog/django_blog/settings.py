607ed391df95e93c792761ea9c51f11f3106d26c
KMuggz <karani.ivan25@gmail.com>
2026-02-14 15:10:15 +0300

    chore: added login redirects

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -127,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# this tells django where to go after successful login/logout
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
