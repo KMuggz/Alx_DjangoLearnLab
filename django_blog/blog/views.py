57e846df319dc04155f9336d39c3034feb7cad53
KMuggz <karani.ivan25@gmail.com>
2026-02-14 15:55:52 +0300

    feat:restructured views.py and added CRUD views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
+from .models import Post
 from .forms import UserRegisterForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+# create your views here
 
+# Registration and Profile Views
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'User Account created for {username}! You can now log in.')
+            messages.success(request, f'Account created for {username}! You can now log in.')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -24,12 +33,55 @@ def profile(request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         if u_form.is_valid():
             u_form.save()
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, 'Your account has been updated!')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'u_form': u_form})
 
-    context = {
-        'u_form': u_form
-    }
-    return render(request, 'blog/profile.html', context)
+# --- Blog Post CRUD Views ---
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # Highlight: Linking the post to the current logged-in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # Highlight: Ensuring the author remains the same during update
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        # Highlight: Security check to ensure only the author can edit
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        # Highlight: Security check to ensure only the author can delete
+        post = self.get_object()
+        return self.request.user == post.author
