28b365183530b8e66a8859ca49e11e26c8f9fa11
KMuggz <karani.ivan25@gmail.com>
2026-02-15 15:54:16 +0300

    chore:added taggit to installed apps

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog', # local app
+    'taggit', # addition for search capacity in the blog
 ]
 
 MIDDLEWARE = [
