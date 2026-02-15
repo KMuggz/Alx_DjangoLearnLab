84172de3e422ea8eae0c7fb173d8b4c622bbb71f
KMuggz <karani.ivan25@gmail.com>
2026-02-15 15:14:30 +0300

    feat:added CommentForm

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -15,3 +16,14 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'rows': 3,
+                'placeholder': 'Add a comment...'
+                }),
+        }
