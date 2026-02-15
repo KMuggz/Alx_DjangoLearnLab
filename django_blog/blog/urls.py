8b6259a565449b83f7ae9e197efbfd730a4effc0
KMuggz <karani.ivan25@gmail.com>
2026-02-15 15:16:25 +0300

    feat: added comment urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,7 +5,10 @@ from .views import (
     PostDetailView, 
     PostCreateView, 
     PostUpdateView, 
-    PostDeleteView
+    PostDeleteView,
+    CommentCreateView, 
+    CommentDeleteView, 
+    CommentUpdateView,
 )
 from django.contrib.auth import views as auth_views
 
@@ -16,10 +19,15 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     
-    # CRUD views
+    # Post CRUD urls
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    
+    # Comment CRUD urls
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
